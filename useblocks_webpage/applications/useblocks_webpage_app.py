5710f097cd8f2fb4d7a44dbdead35547430bdf72
daniel <daniel.woste@useblocks.com>
2016-12-12 15:22:41 +0100

    Fix for / redirect

--- useblocks_webpage/applications/useblocks_webpage_app.py
+++ useblocks_webpage/applications/useblocks_webpage_app.py
@@ -6,19 +6,18 @@ from flask import redirect, url_for
 def create_app():
     app = App([os.path.abspath(os.path.join(os.path.dirname(__file__), "configuration.py"))])
     app.plugins.activate(["GwWeb", "UbWebpageIntroduction", "GwWebManager", "GwPluginsInfo"])
-    return app
-
-
-def start_app():
 
     def open_page():
         with app.web.flask.app_context():
             return redirect(url_for("ub.__introduction_view"))
 
-    app = create_app()
-
     app.web.routes.register("/", ["GET"], open_page, plugin=None, context="web", name="start_redirect")
 
+    return app
+
+
+def start_app():
+    app = create_app()
     app.commands.start_cli()
 
 
